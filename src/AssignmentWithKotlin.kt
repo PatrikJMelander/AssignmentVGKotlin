a981f0ea17c430bc685e2eb414208cc89ca0b58f
PatrikJMelander <patrikjmelander@gmail.com>
2021-01-19 14:27:59 +0100

    test och funkar

--- src/AssignmentWithKotlin.kt
+++ src/AssignmentWithKotlin.kt
@@ -17,21 +17,20 @@ krav är att varje nisse bara får mappas mot dem som är direkt över- och unde
 datastruktur. Direkta mappningar över flera nivåer är inte tillåtna.
 • Skriv en rekursiv funktion där man antingen:
 o Anger en nisses namn och får tillbaka en lista av alla denna nisses chefer, på samtliga
-nivåer. Exempel: Om man skriver in ”Dammråttan” ska en lista innehållandes
-Tomten, Glader, Trötter och Skumtomten returneras
+nivåer. Exempel: Om man skriver in ”Dammråttan” ska en lista innehållande
+Tomten, Glader, Trötter och Skumtomten i retur
 o Anger en nisses namn och får tillbaka en lista på alla dess underordnade, på samtliga
 nivåer. Exempel: Om man skriver in ”Räven” får man tillbaka en lista med
 Gråsuggan, Myran och Bladlusen.
 Inga tomtenamn får vara hårdkodade i den rekursiva loopen (det är ok att hårdkoda i skapandet av
 datastrukturen). Du väljer själv om du skriver koden i Java eller Kotlin.
  */
-fun main(){
-    fun createInputFile(fileName: String): List<String> = File(fileName).readLines()
+fun createInputFile(fileName: String): List<String> = File(fileName).readLines()
+val bosses = mutableListOf<String>()
+val employees = mutableListOf<String>()
+val input = createInputFile("Tomtefil.txt") as MutableList<String>
 
-    val bosses = mutableListOf<String>()
-    val employees = mutableListOf<String>()
-    val input = createInputFile("Tomtefil.txt") as MutableList<String>
-
+fun getBosses (userInput: String){
     fun findBosses(name: String?): List<String>{
         for (lines in input) {
             val firstBreak = lines.indexOf(":")
@@ -46,26 +45,24 @@ fun main(){
         }
         return bosses
     }
+    val listToSend: List<String> = findBosses(userInput)
 
-    fun getBosses (userInput: String){
-        val listToSend: List<String> = findBosses(userInput)
-
-        val printoutResult = StringBuilder()
-        for (s in listToSend) {
-            printoutResult.append(s)
-            printoutResult.append("\n")
-        }
-        println("""
+    val printoutResult = StringBuilder()
+    for (s in listToSend) {
+        printoutResult.append(s)
+        printoutResult.append("\n")
+    }
+    println("""
     ${userInput}s chefer är: $printoutResult
     """.trimIndent())
-    }
-
+}
+fun getEmployees(userInput: String){
     fun findEmployees(name: String): List<String>{
         for (line in input){
             val firstBreak = line.indexOf(":")
             val bossName = line.substring(0, firstBreak)
             val employee = line.substring(firstBreak+2).split(", ")
-            if (name.equals(bossName, ignoreCase = true) )
+            if (name.equals(bossName, true) )
                 for (santa in employee){
                     employees.add(santa)
                     findEmployees(santa)
@@ -73,40 +70,36 @@ fun main(){
         }
         return employees
     }
+    val listToPrint = findEmployees(userInput)
 
-    fun getEmployees(userInput: String){
-        val listToPrint = findEmployees(userInput)
-
-        if (listToPrint.isEmpty())
-            println("$userInput har inga anställda")
-        else{
-            val printResult = StringBuilder()
-            for (name in listToPrint){
-                printResult.append(name)
-                printResult.append("\n");
-            }
-            println("$userInput s anställda är \n$printResult")
+    if (listToPrint.isEmpty())
+        println("$userInput har inga anställda")
+    else{
+        val printResult = StringBuilder()
+        for (name in listToPrint){
+            printResult.append(name)
+            printResult.append("\n")
         }
+        println("$userInput s anställda är \n$printResult")
     }
+}
 
+fun main(){
     while (true) {
         println("Vem vill du söka efter?")
         val userInput = readLine()!!
-
         println("Vill du skriva ut chefer (1) eller anställda (2)")
-        val userChoice = readLine()!!
 
-        when (userInput) {
+        when (readLine()!!) {
             "1" -> getBosses(userInput)
             "2" -> getEmployees(userInput)
             else -> println("Du måste ange 1 eller 2")
         }
 
         println("Vill du söka efter en ny tomte Ja(1) Nej(2)")
-        val userExit = readLine()!!
-        if (userExit == "2")
-            exitProcess(0)
-
+        when (readLine()!!) {
+            "2" -> exitProcess(0)
+        }
         employees.clear()
         bosses.clear()
     }
